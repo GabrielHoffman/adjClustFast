46d06533fbc22d64cafc2665f664dbba614eaa47
Gabriel Hoffman <gabrielhoffman@glia.local>
2021-06-03 22:38:24 -0400

    allow large matricies

--- src/adjClustFast.cpp
+++ src/adjClustFast.cpp
@@ -1,5 +1,6 @@
-// [[Rcpp::depends("RcppArmadillo")]]
+#define ARMA_64BIT_WORD 1
 #include <RcppArmadillo.h>
+// [[Rcpp::depends("RcppArmadillo")]]
 // #include <omp.h>
 
 // write RcppExports.R with: 
